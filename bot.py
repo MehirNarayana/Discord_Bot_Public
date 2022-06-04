1d735fe168748926c967db39d1caabe53057c8be
MehirNarayana <mehirkn@gmail.com>
2022-06-04 17:56:18 -0400

    finished prototype

--- bot.py
+++ bot.py
@@ -1,12 +1,86 @@
-
+from datetime import datetime
+import discord
 from pull import Pull
+from discord.ext import commands
+import asyncio 
+from itertools import cycle
+import json
 
-
+import simplejson
 
 
 data = Pull()
 
-data.return_data()
+
+
+
+
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix = '.', intents=intents)    
+
+
+
+
+async def write_file():
+    while not client.is_closed():
+
+        rows = data.return_data()
+        #client.get_all_members()
+        
+
+        for r in rows:
+            for c in client.get_all_members():
+                if c.nick is None:
+                    disc_nickname = c.name
+                
+                else:
+                    disc_nickname = c.nick
+                
+                row_nickname = r[0]
+                row_id= r[1]
+                
+                disc_id = c.name
+
+                
+
+                if row_nickname in disc_nickname and row_id==disc_id:
+                    
+                    rows.remove(r)
+
+        currtime = datetime.now()    
+        conflicts = [[str(currtime.strftime("%m/%d/%Y, %H:%M:%S"))], [rows]]
+
+        with open('conflicts.txt', 'w') as outfile:
+            simplejson.dump(conflicts, outfile)
+        
+        
+
+       
+        
+        await asyncio.sleep(10)
+
+
+
+@client.event    
+async def on_ready():
+    print('Logged on!')
+
+    
+
+@client.command()
+async def conflicts(ctx):
+    
+    if ctx.author.nick == "Mehir":
+        
+        channel = await ctx.author.create_dm()
+        await channel.send(file=discord.File('./conflicts.txt'))
+        
+
+
+client.loop.create_task(write_file())
+client.run('OTgyNjc4MjUyNTQxODcwMTEw.GpAEVK.Wo1b3HgSSh3vfC6A-mt8VxnJMAssdUq3Jf6n0U')
+
 
 
 
--- pull.py
+++ pull.py
@@ -4,19 +4,22 @@ import gspread as gs
 
 class Pull:
     def __init__(self):
-        self.sheet_id = "1Duo8OpvQAtJEO7y6_gsfykl77wUpi8IkVNyr1Yt4L1I"
-        self.sheet_name = "2021-2022 Robotics Team Signup(responses)"
-        self.url = f"https://docs.google.com/spreadsheets/d/1Duo8OpvQAtJEO7y6_gsfykl77wUpi8IkVNyr1Yt4L1I/edit#gid=1524028929"
+        self.sheet_name = "Sheet1"
+        self.url = 'https://docs.google.com/spreadsheets/d/1Rps9sAo_nU-AxezrMeutLEoNQF86ktgDB4RT0aWrsM8/edit#gid=0'
         
         
 
 
     def return_data(self):
         gc = gs.service_account(filename='robodata-352301-c15537264bd5.json')
-        sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1Rps9sAo_nU-AxezrMeutLEoNQF86ktgDB4RT0aWrsM8/edit#gid=0')
+        sh = gc.open_by_url(self.url)
 
-        ws = sh.worksheet('Sheet1')
+        ws = sh.worksheet(self.sheet_name)
 
         df = pd.DataFrame(ws.get_all_records())
 
-        print(pd.DataFrame(df, columns=['First Name', 'Last Name', 'Best Email to contact you with.']))
+        data = pd.DataFrame(df, columns=['First Name', 'Discord Account Information (username#****)(optional)'])
+
+        rows = data.values.tolist()
+
+        return rows
